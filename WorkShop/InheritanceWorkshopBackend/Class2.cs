ac66c2edd4cbfcad44aac9aeb5f2c87b42846150
JOSE GABRIEL FAJARDO GALVIS <blackone-05@hotmail.com>
2026-09-05 14:24:18 -0500

    class2

---
+++ WorkShop/InheritanceWorkshopBackend/Class2.cs
@@ -0,0 +1,35 @@
+﻿namespace InheritanceWorkshopBackend
+{
+    internal class Circle
+    {
+        //Fields
+        private int _r;
+        
+        public int R
+        { 
+            get; 
+            set; 
+        }
+
+        //Public Methods
+        public int Circle()
+        {
+
+        }
+        public int GetArea()
+        {
+
+        }
+        public int GetPerimeter()
+        {
+
+        }
+
+        //Private Methods
+
+        private int ValidateR()
+        {
+
+        }
+    }
+}
