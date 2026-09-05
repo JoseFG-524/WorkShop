7139e1c32af33b49aac2ad50346e7d22afcee851
JOSE GABRIEL FAJARDO GALVIS <blackone-05@hotmail.com>
2026-09-05 11:58:29 -0500

    pass1

---
+++ WorkShop/InheritanceWorkshopBackend/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace InheritanceWorkshopBackend
+{
+    public class Trapeze
+    {
+        //Fields
+        private int _id;
+        
+        //Constructors
+        public Trapeze()
+        {
+
+        }
+    }
+}
